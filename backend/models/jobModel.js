82e61e02ae305549ac3b584b22ec340fa8d07a34
augusc2 <augusc2@rpi.edu>
2024-10-18 16:24:39 -0400

    added info to job schema

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -18,6 +18,21 @@ const jobSchema = new Schema({
   annualSalary: {
     type: Number,
     required: true
+  },
+  // not required
+  monthSalary: {
+    type: Number,
+    required: false
+  },
+  // in-person, online, or hybrid
+  jobtype: {
+    type: String,
+    required: true
+  },
+  // job description
+  jobDesc: {
+    type: String,
+    required: true
   }
 }, { timestamps: true })
 
