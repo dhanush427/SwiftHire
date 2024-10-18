d617c744a08298ecc1ff3051ae282236079af262
augusc2 <colepaugusta@gmail.com>
2024-10-18 16:57:47 -0400

    updated job details component

--- frontend/src/components/JobDetails.js
+++ frontend/src/components/JobDetails.js
@@ -6,6 +6,9 @@ const JobDetails = ({ job }) => {
         <p><strong>Company: </strong>{job.company}</p>
         <p><strong>Location: </strong>{job.location}</p>
         <p><strong>Annual Salary (USD): </strong>{job.annualSalary}</p>
+        <p><strong>Job Type: </strong>{job.jobType}</p>
+        <p><strong>Monthly Salary (USD): </strong>{job.monthSalary}</p>
+        <p><strong>Job Description: </strong>{job.jobDesc}</p>
         <p>{job.createdAt}</p>
       </div>
     )
