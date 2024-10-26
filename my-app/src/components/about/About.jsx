28cd2f90770963d3f4e06ca3910a48859664e048
Sagar Sahu <sahus@rpi.edu>
2024-10-25 22:52:54 -0400

    Updated homepage design, appearance, and button response

--- my-app/src/components/about/About.jsx
+++ my-app/src/components/about/About.jsx
@@ -3,8 +3,14 @@ import "./about.css";
 
 const About = () => {
     return (
-        <div>About</div>
-    )
-}
+        <section className="about container section" id="about">
+            <div>
+                <p className="description">
+                    SwiftHire is a web application designed to simplify the job application process by gathering information once and enabling users to apply to jobs with a single click. Inspired by the Common App for college applications, SwiftHire aims to make job hunting more efficient and accessible, eliminating the repetitive nature of filling out applications. The platform will provide features like personalized job recommendations, document management, and job search functionalities to empower both job seekers and employers.
+                </p>
+            </div>
+        </section>
+    );
+};
 
-export default About
+export default About;
--- my-app/src/components/home/Home.jsx
+++ my-app/src/components/home/Home.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "./home.css";
+import Me from "../../assets/avatar-1.svg";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className= "home container" id="home">
+            <div className="intro">
+                <img src={Me} alt="" className="home__img" />
+                <h1 className="home__name">
+                    Welcome to SwiftHire!
+                </h1>
+                <span className="home__education">
+                    RCOS Fall 2024
+                </span>
+                <span>{<br/>}</span>
+                <a hfref="#contact" className="btn">
+                    Get Started
+                </a>
+            </div>
+        </section>
     )
 }
 
