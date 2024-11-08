95ee9ce4b2ae92699cddfcc6d4074d5e595b2465
augusc2 <colepaugusta@gmail.com>
2024-11-08 17:15:14 -0500

    added Job Post page/routes

---
+++ my-app/src/pages/JobPost.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const JobPost = () => {
+    return (
+        <main className="main">
+            <h1>Post Job</h1>
+            <form className="post__form">
+                <div className="post__form-name">
+                    <input type="text" className="post__form-name-input" placeholder="Company Name" />
+                </div>
+                <div className="post__form-title">
+                    <input type="text" className="post__form-title-input" placeholder="Role Title" />
+                </div>
+                <div className="post__form-desc">
+                    <input type="text" className="post__form-desc-input" placeholder="Role Description" />
+                </div>
+            </form>
+        </main>
+    )
+}
