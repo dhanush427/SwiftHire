0dd41e6dbb414033c0ce2870c893125ae7e68adc
Dhanush Ananthkar <dhanush427@gmail.com>
2024-12-03 00:11:25 -0500

    updated social tab to only show when scrolled all the way down

--- my-app/src/components/social/Social.jsx
+++ my-app/src/components/social/Social.jsx
@@ -1,34 +1,57 @@
-import React from "react";
-import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
-import './social.css';
+import React, { useState, useEffect } from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import "./social.css";
 
-function SocialMediaLinks() {
-  return (
-    <div className="social-media-container">
-      <ul className="social-media-list">
-        <li>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <FaFacebook className="social-icon" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-            <FaTwitter className="social-icon" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className="social-icon" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-            <FaInstagram className="social-icon" />
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-}
+const SocialMedia = () => {
+    const [show, setShow] = useState(false);
 
-export default SocialMediaLinks;
+    const handleScroll = () => {
+        const windowHeight = window.innerHeight;
+        const scrollTop = window.scrollY;
+        const documentHeight = document.documentElement.scrollHeight;
+
+        const isAtBottom = windowHeight + scrollTop >= documentHeight - 1; // Check if scrolled to the bottom
+        setShow(isAtBottom);
+
+        console.log({ windowHeight, scrollTop, documentHeight, isAtBottom }); // Debugging output
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className={`social-media-container ${show ? "show" : ""}`}>
+            <ul className="social-media-list">
+                <li>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook className="social-icon" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter className="social-icon" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram className="social-icon" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="social-icon" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default SocialMedia;
+
+
