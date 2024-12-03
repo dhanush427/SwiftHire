8d4574863057186a4fec9ef51d817e8cac5d1bae
Dhanush Ananthkar <dhanush427@gmail.com>
2024-12-03 00:17:55 -0500

    updated ui for the sidebar to have animations and center

--- my-app/src/components/sidebar/Sidebar.jsx
+++ my-app/src/components/sidebar/Sidebar.jsx
@@ -1,49 +1,48 @@
 import React from "react";
 import "./sidebar.css";
 import Logo from "../../assets/logo.svg";
+import { FaHome, FaUser, FaBriefcase, FaEnvelope, FaLayerGroup } from "react-icons/fa";
 
 const Sidebar = () => {
     return (
         <aside className="aside">
-            <a href="#home" className="nav__logo">
-                <img src={Logo} alt="" />
-            </a>
+            <div className="logo-container">
+                <a href="#home" className="nav__logo">
+                    <img src={Logo} alt="Logo" />
+                </a>
+            </div>
 
             <nav className="nav">
-                <div className="nav__menu">
-                    <ul className="nav__list">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link">
-                                <i className="icon-home"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <i className="icon-user"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#services" className="nav__link">
-                                <i className="icon-notebook"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="icon-note"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#account" className="nav__link">
-                                <i className="icon-layers"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                <ul className="nav__list">
+                    <li className="nav__item">
+                        <a href="#home" className="nav__link">
+                            <FaHome className="nav__icon" />
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#about" className="nav__link">
+                            <FaUser className="nav__icon" />
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#services" className="nav__link">
+                            <FaBriefcase className="nav__icon" />
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#contact" className="nav__link">
+                            <FaEnvelope className="nav__icon" />
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#account" className="nav__link">
+                            <FaLayerGroup className="nav__icon" />
+                        </a>
+                    </li>
+                </ul>
             </nav>
-
-            
         </aside>
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
